609c68a561f4057888a8c8fe325d97109af3544b
KenjiDoom <vitodevcool@protonmail.com>
2021-05-13 17:42:00 -0700

    Okay the for loop now working

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 # Author: KenjiDoom
-
+import requests
 
 def main():
-    pass
+    #wordlist = input("Enter wordlist: ")
+    wordlist = ['list', 'test', 'yikes', 'and', 'okay?']
+    for words in wordlist:
+        print('www.google.com/' + words)
+
+#if r.status_code == 200:
+ #   print("working")
+#elif r.status_code == 404:
+  #  print('Not found')
+#else:
+ #   print("I have no clue")
+main()
+
